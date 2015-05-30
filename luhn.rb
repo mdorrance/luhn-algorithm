cad0a0c83327fdab4ccefa3da4e85cce022c73b1
Michael Dorrance <dorrancemd@gmail.com>
2015-05-30 13:40:16 -0600

    working tdd version of credit card check

---
+++ luhn.rb
@@ -0,0 +1,73 @@
+class Luhn
+
+	def initialize(card)
+		@card = card
+	end
+
+	def reversed_card
+		new_card_number = @card.reverse
+		new_card_number.chars.map(&:to_i)
+	end
+
+
+
+	def double_every_other_one
+		 numbers = reversed_card.map.with_index do |number, index|		
+			if (index + 1) % 2 == 0
+				(number * 2)				
+			else 
+				number				
+			end		
+		 end
+	end
+
+	def adding_length_of_two
+		double_every_other_one.map do |number|
+			if number.to_s.length == 2
+				number - 9
+			else
+				number
+			end
+		end
+	end
+
+	def sum_all_numbers
+		adding_length_of_two.reduce(0) do |sum, number|
+			sum + number
+		end
+	end
+
+	def check_validity?
+		if sum_all_numbers % 10 == 0
+			puts "Credit Card is valid."
+			true
+		else
+			puts "Credit Card is FAKE."
+			false
+		end
+	end
+
+end
+
+
+test1 = Luhn.new("4929735477250543")
+test1.check_validity?
+test2 = Luhn.new("5541801923795240")
+test2.check_validity?
+test3 = Luhn.new("5541808923795240")
+test3.check_validity?
+test4 = Luhn.new("4024007106512380")
+test4.check_validity?
+test5 = Luhn.new("6011797668867828")
+test5.check_validity?
+
+#take a number and starting from the right, take every other number and double it
+#if the double is > 9 then split it and add the individual numbers
+#then sum all the numbers and % 10 with remainder 0
+
+
+# Your Luhn Algorithm Here
+
+# Output
+## If it is valid, print "The number is valid!"
+## If it is invalid, print "The number is invalid!"
---
+++ luhn_test.rb
@@ -0,0 +1,32 @@
+require 'minitest'
+require 'minitest/autorun'
+require './luhn'
+
+class LuhnTest < Minitest::Test
+	def setup
+		@luhn = Luhn.new("4929735477250543")
+	end
+	def test_reverses_string_and_changes_to_integer
+		result = @luhn.reversed_card
+		assert_equal [3, 4, 5, 0, 5, 2, 7, 7, 4, 5, 3, 7, 9, 2, 9, 4], result
+	end
+
+	def test_double_every_other_one
+		result = @luhn.double_every_other_one
+		assert_equal [3, 8, 5, 0, 5, 4, 7, 14, 4, 10, 3, 14, 9, 4, 9, 8], result
+	end
+
+	def test_for_length_of_2
+		result = @luhn.adding_length_of_two
+		assert_equal [3, 8, 5, 0, 5, 4, 7, 5, 4, 1, 3, 5, 9, 4, 9, 8], result
+
+	end
+	def test_sum_all_numbers
+		result = @luhn.sum_all_numbers
+		assert_equal 80, result
+	end
+	def test_validity
+		result = @luhn.check_validity?
+		assert result
+	end
+end
